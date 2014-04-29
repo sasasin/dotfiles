f5f243d5e11bb83c21104a979410ae3f7aa8dd9d
Shinnosuke Suzuki <sasasin@sasasin.net>
2014-04-29 21:25:48 +0900

    gitconfigに、認証設定を追加。make_symlinkを微調整。

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-ln -s ~/git/dotfiles/.emacs.d/ ~/.emacs.d
+
+rm -f ~/.bashrc
+rm -f ~/.gitconfig
+rm -f ~/.gtk-bookmarks
 
 ln -s ~/git/dotfiles/.bashrc ~/.bashrc
 ln -s ~/git/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/git/dotfiles/.gtk-bookmarks ~/.gtk-bookmarks 
 
+ln -s ~/git/dotfiles/.emacs.d/ ~/.emacs.d
 ln -s ~/git/dotfiles/script ~/script
+
