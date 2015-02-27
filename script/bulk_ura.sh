32dd5e2d3386c555c67aedf7a28248920eb14927
shinnosuke.suzuki <sasasin@sasasin.net>
2015-02-28 01:46:59 +0900

    並列数の調整

--- script/bulk_ura.sh
+++ script/bulk_ura.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ls *[13579].jpg \
-| xargs -P8 -L3 hosei_ura.sh
+| xargs -P1 -L3 hosei_ura.sh
 
 ls *.jpg \
-| xargs -P8 -L3 hosei_ura.sh
+| xargs -P1 -L3 hosei_ura.sh
--- script/hosei_zassi.sh
+++ script/hosei_zassi.sh
@@ -4,10 +4,10 @@ ads2500w_rename.sh *_*.jpg
 #ls *.jpg | xargs -P8 -L3 hosei_ura.sh
 #ls *[02468].jpg | xargs -P8 -L3 hosei_ura.sh
 
-ls *.jpg | xargs -P8 -L3 hosei_white.sh
+ls *.jpg | xargs -P1 -L3 hosei_white.sh
 
-ls *.jpg | xargs -P8 -L3 mogrify -trim
+ls *.jpg | xargs -P1 -L3 mogrify -trim
 
-#ls *.jpg | xargs -P8 -L3 hosei_sharpen.sh
+#ls *.jpg | xargs -P1 -L3 hosei_sharpen.sh
 
 #make_jisui_pdf.sh $(get_timestamp.sh)
