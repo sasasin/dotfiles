c9f44e262a0e752eecefe163516d8b888a1a4833
shinnosuke.suzuki <sasasin@sasasin.net>
2015-07-26 15:20:40 +0900

    並列実行されるようにした。

--- script/hosei_rotate.sh
+++ script/hosei_rotate.sh
@@ -1,14 +1,15 @@
 #!/bin/bash -v
 ads2500w_rename.sh *.jpg
 wait
-hosei_rotate90.sh *0.jpg 
-hosei_rotate90.sh *2.jpg 
-hosei_rotate90.sh *4.jpg 
-hosei_rotate90.sh *6.jpg 
-hosei_rotate90.sh *8.jpg 
-hosei_rotate270.sh *1.jpg 
-hosei_rotate270.sh *3.jpg 
-hosei_rotate270.sh *5.jpg 
-hosei_rotate270.sh *7.jpg 
-hosei_rotate270.sh *9.jpg 
+hosei_rotate90.sh *0.jpg &
+hosei_rotate90.sh *2.jpg &
+hosei_rotate90.sh *4.jpg &
+hosei_rotate90.sh *6.jpg &
+hosei_rotate90.sh *8.jpg &
+wait
+hosei_rotate270.sh *1.jpg &
+hosei_rotate270.sh *3.jpg &
+hosei_rotate270.sh *5.jpg &
+hosei_rotate270.sh *7.jpg &
+hosei_rotate270.sh *9.jpg &
 wait
