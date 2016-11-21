e6103bd681272e17bfa9951ecaab7868493b6173
Shinnosuke Suzuki <sasasin@sasasin.net>
2016-11-21 23:44:54 +0900

    ubuntu-ja-non-freeがエラーなので外した

--- script/startup.sh
+++ script/startup.sh
@@ -7,11 +7,11 @@ LANG=C xdg-user-dirs-gtk-update
 apt-get install -y cifs-utils fuse-utils preload
 
 # add ubuntu japanese team
-sudo wget https://www.ubuntulinux.jp/sources.list.d/${V_NAME}.list \
-    -O /etc/apt/sources.list.d/ubuntu-ja.list
 rm -f /etc/apt/sources.list.d/ubuntu-ja.list
 wget -q https://www.ubuntulinux.jp/ubuntu-ja-archive-keyring.gpg -O- | sudo apt-key add -
 wget -q https://www.ubuntulinux.jp/ubuntu-jp-ppa-keyring.gpg -O- | sudo apt-key add -
+echo "deb http://archive.ubuntulinux.jp/ubuntu ${V_NAME} main" > /etc/apt/sources.list.d/ubuntu-ja.list
+#echo "deb http://archive.ubuntulinux.jp/ubuntu-ja-non-free ${V_NAME} multiverse" >> /etc/apt/sources.list.d/ubuntu-ja.list
 
 # add virtualbox repository
 wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- \
