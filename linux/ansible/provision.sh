c68b95c2a6f5cbd7958ad38d56317953726c41ce
Shinnosuke Suzuki <sasasin@sasasin.net>
2016-12-17 18:36:26 +0900

    ansibleでセットアップするようにしてみた

---
+++ linux/ansible/provision.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -v
+# install ansible with dependent packages
+apt-get -y install python2.7 python-setuptools lsb
+apt-get -y ieee-data python-httplib2 python-jinja2 python-markupsafe python-netaddr python-selinux python-yaml
+pip install ansible
+
+ansible-galaxy install -p ./roles -f -r requirements.yml
+#ansible -i localhost.inv default -m setup --connection=local
+ansible-playbook -i localhost.inv localhost.yml --connection=local
+sudo -iu $SUDO_USER /bin/bash -lc '/usr/bin/im-config -n fcitx'
+sudo -iu $SUDO_USER /bin/bash -lc 'LANG=C xdg-user-dirs-gtk-update'
