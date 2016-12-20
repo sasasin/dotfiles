0a9fdb6816d0951d81825cc026debf30e18c16b9
Shinnosuke Suzuki <sasasin@sasasin.net>
2016-12-20 16:22:03 +0900

    need root priv

--- linux/ansible/provision.sh
+++ linux/ansible/provision.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -v
+if [ "`whoami`" != "root" ]; then
+  echo "Require root privilege"
+  exit 1
+fi
 # install ansible with dependent packages
 apt-get -y install python2.7 python-setuptools lsb
 apt-get -y ieee-data python-httplib2 python-jinja2 python-markupsafe python-netaddr python-selinux python-yaml
