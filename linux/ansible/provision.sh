d09815f63ce0bdb740da2d6edc3a91107ba6d88b
Shinnosuke Suzuki <sasasin@sasasin.net>
2017-06-20 11:01:53 +0900

    ansibleはapt-getで入れることにした

--- linux/ansible/provision.sh
+++ linux/ansible/provision.sh
@@ -4,11 +4,11 @@ if [ "`whoami`" != "root" ]; then
   exit 1
 fi
 # install ansible with dependent packages
-apt-get -y install python2.7 python2.7-dev python-setuptools lsb
+apt-get -y install ansible python2.7 python2.7-dev python-setuptools lsb
 apt-get -y install ieee-data python-httplib2 python-jinja2 python-markupsafe python-netaddr python-selinux python-yaml
 easy_install pip
-pip2 install ansible
-pip2 install docker-py
+#pip2 install ansible
+#pip2 install docker-py
 
 easy_install3 pip
 pip3 install --upgrade git+https://github.com/yadayada/acd_cli.git
