cfa9079433c5c7b06985dd0eb4c47f43fe48d1cc
Shinnosuke Suzuki <sasasin@sasasin.net>
2017-10-20 12:16:50 +0900

    ゴミ掃除

--- linux/ansible/provision.sh
+++ linux/ansible/provision.sh
@@ -7,13 +7,13 @@ fi
 apt-get -y install ansible python2.7 python2.7-dev python-setuptools lsb
 apt-get -y install ieee-data python-httplib2 python-jinja2 python-markupsafe python-netaddr python-selinux python-yaml
 easy_install pip
-#pip2 install ansible
+pip2 install ansible
 #pip2 install docker-py
+#pip2 install awscli
+#pip2 install awsebcli
 
-easy_install3 pip
-pip3 install --upgrade git+https://github.com/yadayada/acd_cli.git
-pip3 install awscli
-pip3 install awsebcli
+#easy_install3 pip
+#pip3 install --upgrade git+https://github.com/yadayada/acd_cli.git
 
 add-apt-repository -yu ppa:snwh/pulp
 
@@ -23,4 +23,4 @@ ansible-playbook -i localhost.inv localhost.yml --connection=local
 sudo -iu $SUDO_USER /bin/bash -lc '/usr/bin/im-config -n uim'
 sudo -iu $SUDO_USER /bin/bash -lc 'LANG=C xdg-user-dirs-gtk-update'
 
-pip3 install docker-compose
+pip2 install docker-compose
