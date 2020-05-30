5a902e88fe8e01403dad1a9dee66747b99646af8
sasasin <sasasin@sasasin.net>
2020-05-31 06:27:13 +0900

    ゴミゴミしてるので

--- linux/ansible/provision.sh
+++ linux/ansible/provision.sh
@@ -4,6 +4,10 @@ if [ "`whoami`" != "root" ]; then
   exit 1
 fi
 
+# ansible内ではうまく取り込めないようなので
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+
 ansible-galaxy install -p ./roles -f -r requirements.yml
 #ansible -i localhost.inv default -m setup --connection=local
 ansible-playbook -i localhost.inv localhost.yml --connection=local --diff -vv
