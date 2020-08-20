460a5efb491a0f6c43b5f4e275e9bdc87502e27b
sasasin <sasasin@sasasin.net>
2020-08-20 10:11:51 +0900

    地味

--- linux/ansible/provision.sh
+++ linux/ansible/provision.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -v
+#!/bin/bash -ve
 if [ "`whoami`" != "root" ]; then
   echo "Require root privilege"
   exit 1
@@ -11,6 +11,7 @@ wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key a
 ansible-galaxy install -p ./roles -f -r requirements.yml
 #ansible -i localhost.inv default -m setup --connection=local
 ansible-playbook -i localhost.inv localhost.yml --connection=local --diff -vv
+
 sudo -iu $SUDO_USER /bin/bash -lc 'LANG=C xdg-user-dirs-update --force'
 sudo -iu $SUDO_USER /bin/bash -lc 'gsettings set org.gnome.desktop.wm.preferences mouse-button-modifier "<Super>"'
 
