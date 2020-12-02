c6c5cc2b0d5c3ace3533d84a01ec7f68397bc79a
sasasin <sasasin@sasasin.net>
2020-12-03 02:53:08 +0900

    python3

--- linux/ansible/install-ansible.sh
+++ linux/ansible/install-ansible.sh
@@ -6,5 +6,6 @@ fi
 # install ansible with dependent packages
 apt-get -y install python3-pip lsb
 apt-get -y install ieee-data python3-httplib2 python3-jinja2 python3-markupsafe python3-netaddr python3-selinux python3-yaml
-pip3 install -U ansible
+update-alternatives --install /usr/bin/python python /usr/bin/python3 1
+pip install -U ansible
 
