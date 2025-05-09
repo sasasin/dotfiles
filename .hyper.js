8f1968d88b579426ef5252dd88c7850b442f9777
shinnosuke.suzuki <sasasin@sasasin.net>
2025-05-09 16:34:47 +0900

    change Hyper terminal font

--- .hyper.js
+++ .hyper.js
@@ -11,7 +11,7 @@ module.exports = {
         fontSize: 18,
         // font family with optional fallbacks
         //fontFamily: 'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
-        fontFamily: '"HackGen35 Console NF"',
+        fontFamily: '"UDEV Gothic 35NFLG"',
         // default font weight: 'normal' or 'bold'
         fontWeight: 'normal',
         // font weight for bold characters: 'normal' or 'bold'
@@ -138,7 +138,7 @@ module.exports = {
     //   `hyperpower`
     //   `@company/project`
     //   `project#1.0.1`
-    plugins: ["hyper-one-light"],
+    plugins: ["hyper-relaxed"],
     // in development, you can create a directory under
     // `~/.hyper_plugins/local/` and include it here
     // to load it and avoid it being `npm install`ed
