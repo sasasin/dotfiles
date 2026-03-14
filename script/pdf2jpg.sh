e60972c578cb5c849076bf2e8b717fd8d852a580
sasasin <sasasin@sasasin.net>
2026-03-14 21:55:00 +0900

    feat: Remove unused 'pdf2jpg.sh' conversion script

--- script/pdf2jpg.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-ls "$@" | while read f; do
-    DIRNAME=$(basename "$f" ".pdf")
-    echo "converting $f to $DIRNAME"
-    rm -rf "$DIRNAME"
-    mkdir "$DIRNAME"
-    pdfimages -j "$f" "$DIRNAME"/"DIRNAME"
-done
