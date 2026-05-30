5d007f94ed42004d0ae417add3f8957725c37957
ShinnosukeSuzuki <sasasin@sasasin.net>
2026-05-31 06:10:02 +0900

    Update darwin bash config

--- .config/bash/os/darwin.bash
+++ .config/bash/os/darwin.bash
@@ -38,8 +38,6 @@ prepend_path /usr/share/nodejs/yarn/bin
 prepend_path "$HOME/.linkerd2/bin"
 prepend_path "$HOME/.rd/bin"
 
-export GHQ_SELECTOR=peco
-
 export QT_AUTO_SCREEN_SCALE_FACTOR=2
 
 export AWS_DEFAULT_REGION=ap-northeast-1
@@ -49,36 +47,8 @@ export AWS_VAULT_BACKEND=keychain
 export AWS_VAULT_PASS_PREFIX=aws-vault
 export AWS_SESSION_TOKEN_TTL=3h
 
-# https://developer.1password.com/docs/cli/app-integration
-export OP_BIOMETRIC_UNLOCK_ENABLED=true
-
-# export KUBECONFIG="$HOME/k8s.home.sasasin.net.kubeconfig.yml:$HOME/.kube/config"
-# export DOCKER_HOST=ssh://limadocker:60006
-
 alias mi='micro'
-# alias pbcopy='xclip -selection c'
-# alias pbpaste='xclip -selection c -o'
-
-peco_select_history() {
-  local selected
-
-  selected="$(
-    HISTTIMEFORMAT= history |
-      sed 's/^ *[0-9]\+ *//' |
-      awk '!seen[$0]++' |
-      peco --query "$READLINE_LINE"
-  )" || return
-
-  [[ -n "$selected" ]] || return
-  READLINE_LINE="$selected"
-  READLINE_POINT=${#READLINE_LINE}
-}
-
-if [[ $- == *i* ]] && command -v peco >/dev/null 2>&1; then
-  bind -x '"\C-r": peco_select_history'
-fi
 
 unset -f prepend_path
 
 # fzf --bash
-# starship is initialized from ~/.config/bash/conf.d/starship.bash.
