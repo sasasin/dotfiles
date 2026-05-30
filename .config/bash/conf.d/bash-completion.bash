fc8c154d757747d5dc24ad6cc4fc8dc0f3e7d399
ShinnosukeSuzuki <sasasin@sasasin.net>
2026-05-31 06:11:45 +0900

    Guard bash completion source

--- .config/bash/conf.d/bash-completion.bash
+++ .config/bash/conf.d/bash-completion.bash
@@ -3,4 +3,5 @@
 # bash-completion は winget や scoop で配布されていない。
 # ghq get https://github.com/scop/bash-completion
 # したものを source することにした
-. ~/src/github.com/scop/bash-completion/bash_completion
+# macOS は homebrew で入手したものを利用する
+[[ -f ~/src/github.com/scop/bash-completion/bash_completion ]] && . ~/src/github.com/scop/bash-completion/bash_completion
